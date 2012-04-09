4aaecc8ea89405a33a64821a7396c56e0ac54f4d
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2012-04-09 15:33:35 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+April9JackmodoApp::Application.config.secret_token = '243bfa938118c40cffd9405a49659dd47b28482eacc769102e77b95ff3be566ea23c1b73f1fe58ca2c49e4ce3e5a343ac86cd932b18cdf84c262633a4549b65a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+April9JackmodoApp::Application.config.session_store :cookie_store, key: '_april9_jackmodo_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# April9JackmodoApp::Application.config.session_store :active_record_store
